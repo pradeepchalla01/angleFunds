882967fd580fe855317cf19bcb6472ec2458f826
teamdev49 <msraju531@gmail.com>
2016-05-23 16:05:08 +0530

    --Adding ionic date picker and validations in newPatient page

--- www/js/user/patients/NewPatientCtrl.js
+++ www/js/user/patients/NewPatientCtrl.js
@@ -1,45 +1,72 @@
 angular.module('angleFunds')
 
 .controller('NewPatientCtrl', function($log, $scope, $state, newPatientService) {
-	$log.debug('New Patient');
 	$scope.newPatient = {};
 	$scope.errors = {};
 	$scope.organizations = ['organization 1', 'organization 2', 'organization3'];
 	$scope.organization= ['organization 1'];
 	  
-  	$scope.toggleSelection = function toggleSelection(organization) {
-    	var idx = $scope.organization.indexOf(organization);
+  	$scope.toggleSelection = function toggleSelection(org) {
+    	var idx = $scope.organization.indexOf(org);
 
 	    if (idx > -1) {
 	      $scope.organization.splice(idx, 1);
 	    }
     	// is newly selected
 	    else {
-	      $scope.organization.push(organization);
+	      $scope.organization.push(org);
 	    }
   	};
+
+	$scope.newPatient.onezoneDatepicker = {
+	    date: new Date(), // MANDATORY                     
+	};
 
 	$scope.savePatientDetails = function(){
 		$scope.errors.newPatient= {};
 		$scope.newPatient.organization = $scope.organization;
-			$log.debug('inside savePatientDetails');
 			if(!$scope.newPatient.name){
 				$scope.errors.newPatient.name = 'please enter patient name';
 			} 
-			if(!$scope.newPatient.dob){
-				$scope.errors.newPatient.dob = 'please enter patient date of birth';
+			if(!$scope.newPatient.onezoneDatepicker){
+				$scope.errors.newPatient.onezoneDatepicker = 'please enter patient date of birth';
 			}
 			if(!$scope.newPatient.parentName){
 				$scope.errors.newPatient.parentName = "please enter patient's parent name";
 			}
 			if(!$scope.newPatient.phone){
 				$scope.errors.newPatient.phone = "please enter valid phone number";
+			}else{
+				var evt = $scope.newPatient.phone;
+				if(!isNaN(evt)){
+					if(!(evt.length == 10)){
+						$scope.errors.newPatient.phone = "Phone number is incorrect";	
+					}
+				}else{
+					$scope.errors.newPatient.phone = "please enter numbers only";		
+				}
 			}
 			if(!$scope.newPatient.alterPhone){
 				$scope.errors.newPatient.alterPhone = "please enter Alternate phone number";
+			}else{
+				var evt = $scope.newPatient.alterPhone;
+				if(!isNaN(evt)){
+					if(!(evt.length == 10)){
+						$scope.errors.newPatient.alterPhone = "Phone number is incorrect";	
+					}
+				}else{
+					$scope.errors.newPatient.alterPhone = "please enter numbers only";		
+				}
 			}
 			if(!$scope.newPatient.email){
 				$scope.errors.newPatient.email = "please enter email address";
+			}else{
+			    var x = $scope.newPatient.email;
+			    var atpos = x.indexOf("@");
+			    var dotpos = x.lastIndexOf(".");
+			    if (atpos<1 || dotpos<atpos+2 || dotpos+2>=x.length) {
+			        $scope.errors.newPatient.email = "please enter email address";
+			    }
 			}
 			if(!$scope.newPatient.doctorName){
 				$scope.errors.newPatient.doctorName = "please enter doctorn name";
@@ -58,9 +85,19 @@ angular.module('angleFunds')
 			}
 			if(!$scope.newPatient.appCost){
 				$scope.errors.newPatient.appCost = "please enter approximate cost";
+			}else {
+				var appCost = $scope.newPatient.appCost;
+				if(isNaN(appCost)){
+					$scope.errors.newPatient.appCost = "please enter approximate cost in numbers ";	
+				}
 			}
 			if(!$scope.newPatient.familyIncome){
 				$scope.errors.newPatient.familyIncome = "please enter monthly family income";
+			}else {
+				var familyIncome = $scope.newPatient.familyIncome;
+				if(isNaN(familyIncome)){
+					$scope.errors.newPatient.familyIncome = "please enter familyIncome in numbers ";	
+				}
 			}
 			if(!$scope.newPatient.occupation){
 				$scope.errors.newPatient.occupation = "please enter atleast one occupation";
@@ -71,8 +108,8 @@ angular.module('angleFunds')
 			if($scope.organization.length > 0){
 				$scope.errors.newPatient.organization = "please select atleast one organization";
 			}
-			$log.debug($scope.newPatient);
 
+			//calling the service method
 			if(angular.equals({}, $scope.errors.newPatient)) {
 				newPatientService.setAddNewPatientDetails($scope.newPatient)
 					 .then(function(){
@@ -83,7 +120,6 @@ angular.module('angleFunds')
 				
 			}
 		}
-
 
 	$scope.reset =  function(){
 		$scope.newPatient = {};
